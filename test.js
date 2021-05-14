0f94c71345196e28681bf5e05b87003635a8a569
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-05-14 18:17:41 +0200

    fix lock

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ const post = require('./main.js')
 const fs = require('fs');
 fs.readFile('./ids.json', 'utf-8', (err, data) => {
     ids = JSON.parse(data)
-    post(ids.login, ids.password, __dirname + '/test.mp4', 'Hihihi3 #dev #meme #memes #memes😂')
+    post(ids.login, ids.password, __dirname + '/test.mp4', 'Hihihi4 #dev #meme #memes #memes😂', false)
 })
