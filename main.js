13281a5e331a7150b56bebbc8c2d33ccf7ea120d
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-06-28 16:15:34 +0200

    add more logs

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer-extra')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
 
 /**
  * @typedef {Function} LogFunction
@@ -28,7 +30,7 @@ function post(
         const browser = await puppeteer.launch({
             headless: ! show,
             args: [
-                '--window-size=800,500',
+                '--window-size=1000,800',
                 //'--window-position=0,-600'
             ]
         })
@@ -44,7 +46,9 @@ function post(
 
         sendLog('Go to login page')
         const page = await browser.newPage()
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 OPR/77.0.4054.60');
         await page.goto('https://www.tiktok.com/login')
+
         sendLog('Waiting for Fb Login selector...')
 
         const facebookButtonSelector = '.channel-item-wrapper-2gBWB+.channel-item-wrapper-2gBWB+.channel-item-wrapper-2gBWB .channel-name-2qzLW'
@@ -56,7 +60,7 @@ function post(
         await browser.on('targetcreated', async () => {
             sendLog('Target created')
             /** @type {import('puppeteer').Page} foundFacebookLogin */
-            const facebookLoginPage = await findFacebookLogin(browser);
+            const facebookLoginPage = await findFacebookLogin(browser, sendLog);
             sendLog('Fb login page found ? ' + (facebookLoginPage ? 'yes' : 'no'))
             if (facebookLoginPage) {
                 sendLog('Fb login page reloading...')
@@ -79,7 +83,7 @@ function post(
                 sendLog('Likely logged in !')
             } else {
                 /** @type {import('puppeteer').Page} loggedInPage */
-                const loggedInPage = await findLoggedInPage(browser);
+                const loggedInPage = await findLoggedInPage(browser, sendLog);
                 sendLog('TikTok page found ? ' + (loggedInPage ? 'yes' : 'no'))
                 if (loggedInPage) {
                     if (! hasLoggedIn) {
@@ -122,8 +126,18 @@ function post(
                             sendLog('Clicked !')
                             setTimeout(async () => {
                                 await page.waitForTimeout(10000)
+
                                 const goToProfileButton = '.modal-btn+.modal-btn'
-                                await page.waitForSelector(goToProfileButton)
+                                try {
+                                    await page.waitForSelector(goToProfileButton)
+                                } catch (error) {
+                                    sendLog(await page.evaluate(() => document.head.outerHTML + document.body.outerHTML))
+                                    await browser.close()
+                                    rejects('Profile button not found after posting : ' + error.message)
+
+                                    return
+                                }
+
                                 await page.click(goToProfileButton)
 
                                 const lastVideoSelector = '.video-feed-item-wrapper'
@@ -143,44 +157,61 @@ function post(
             }
         })
 
+        sendLog('Clicking Fb Login button !')
         await page.click(facebookButtonSelector)
+        sendLog('Fb Login button clicked !')
     })
 }
 
 /**
- * @param {import('puppeteer').Browser} browser 
+ * @param {import('puppeteer').Browser} browser
+ * @param {LogFunction} sendLog
  * 
  * @returns {?import('puppeteer').Page}
  */
-async function findFacebookLogin(browser) {
-    return await findPageIncludes(browser, 'facebook.com/login.php')
+async function findFacebookLogin(browser, sendLog) {
+    return await findPageIncludes(browser, 'facebook.com/login.php', sendLog)
 }
 
 /**
- * @param {import('puppeteer').Browser} browser 
+ * @param {import('puppeteer').Browser} browser
+ * @param {LogFunction} sendLog
  * 
  * @returns {?import('puppeteer').Page}
  */
-async function findLoggedInPage(browser) {
+async function findLoggedInPage(browser, sendLog) {
     return await findPageIncludes(
         browser,
-        'https://www.tiktok.com/foryou?loginType=facebook&lang=en'
+        'https://www.tiktok.com/foryou?loginType=facebook&lang=en',
+        sendLog
     ) || await findPageIncludes(
         browser,
-        'https://www.tiktok.com/foryou?lang=en'
+        'https://www.tiktok.com/foryou?lang=en',
+        sendLog
+    ) || await findPageIncludes(
+        browser,
+        'https://www.tiktok.com/foryou?loginType=facebook&lang=fr',
+        sendLog
+    ) || await findPageIncludes(
+        browser,
+        'https://www.tiktok.com/foryou?lang=fr',
+        sendLog
     )
 }
 
 /**
  * @param {import('puppeteer').Browser} browser 
  * @param {string} pageTitleIncludedText
+ * @param {LogFunction} sendLog
  * 
  * @returns {?import('puppeteer').Page}
  */
-async function findPageIncludes(browser, pageTitleIncludedText) {
+async function findPageIncludes(browser, pageTitleIncludedText, sendLog) {
     let pages = await browser.pages()
     for (let i = 0; i < pages.length; i += 1) {
-        if (pages[i].url().includes(pageTitleIncludedText)) {
+        const pageUrl = pages[i].url()
+        sendLog(pageUrl)
+        if (pageUrl.includes(pageTitleIncludedText)) {
             module.exports.page = pages[i] // Set the new working page as the popup
             return pages[i]
         }
--- test.js
+++ test.js
@@ -2,5 +2,12 @@ const post = require('./main.js')
 const fs = require('fs');
 fs.readFile('./ids.json', 'utf-8', (err, data) => {
     ids = JSON.parse(data)
-    post(ids.login, ids.password, __dirname + '/test.mp4', 'Hihihi11 #dev', false).then(console.log)
+    post(
+        ids.login,
+        ids.password,
+        __dirname + '/test.mp4',
+        'Hihihi14 #dev',
+        false,
+        (toLog) => console.log(toLog)
+    ).then(console.log)
 })
