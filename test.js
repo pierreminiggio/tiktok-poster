3d77fd71b942cb13d3aefdd65543db54081dc2f9
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-06-28 11:28:51 +0200

    update puppeteer

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ const post = require('./main.js')
 const fs = require('fs');
 fs.readFile('./ids.json', 'utf-8', (err, data) => {
     ids = JSON.parse(data)
-    post(ids.login, ids.password, __dirname + '/test.mp4', 'Hihihi10 #dev', true).then(console.log)
+    post(ids.login, ids.password, __dirname + '/test.mp4', 'Hihihi11 #dev', false).then(console.log)
 })
